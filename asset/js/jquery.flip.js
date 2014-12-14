032b7e61a76a201b9ccd543a6c7e5e61a2975704
Nattawat Nonsung <armmer1@gmail.com>
2014-12-14 13:50:03 +1100

    Update jquery.flip

--- asset/js/jquery.flip.js
+++ asset/js/jquery.flip.js
@@ -5,103 +5,102 @@
  * Copyright 2014, Nattawat Nonsung
  */
 
-
-/**
-* Use immediately Invoked Function Expression to
-* - Prevent conflicting with other libary on alias $
-* - Scope varaible to be private
-*/
 (function( $ ) {
-	var flip = function(dom, flipedRotate){
-		dom.data("fliped", true);
-		dom.css({
-			transform: flipedRotate
-		});
-	};
-	var unflip = function(dom){
-		dom.data("fliped", false);
-		dom.css({
-			transform: "rotatex(0deg)"
-		});
-	};
+  var flip = function(dom, flipedRotate) {
+    dom.data("fliped", true);
+    dom.css({
+      transform: flipedRotate
+    });
+  };
+  var unflip = function(dom) {
+    dom.data("fliped", false);
+    dom.css({
+      transform: "rotatex(0deg)"
+    });
+  };
   $.fn.flip = function(options) {
-  	var jqObj = this;
-    var width = this.width();
-		var height = this.height();
-		var prespective = width*2;
+    this.each(function(){
+      var $dom = $(this);
+      var width = $dom.width();
+      var height = $dom.height();
+      var margin = $dom.css('margin');
+      var prespective;
 
-  	if(options !== undefined && typeof(options) == "boolean"){ // Force flip the DOM
-  		if(options){
-  			flip(this, this.data("flipedRotate"));
-  		}else{
-  			unflip(this);
-  		}
-  	}else{ //Init flipable DOM
-  		// Define default setting
-			var settings = $.extend({
-				axis: "y",
-				trigger: "click"
-	    }, options );
+      if (options !== undefined && typeof(options) == "boolean") { // Force flip the DOM
+        if (options) {
+          flip($dom, $dom.data("flipedRotate"));
+        } else {
+          unflip($dom);
+        }
+      } else { //Init flipable DOM
+        var settings = $.extend({
+          axis: "y",
+          trigger: "click"
+        }, options );
 
-			if(settings.axis.toLowerCase() == "x"){
-				prespective = height*2;
-				// save rotating css to DOM for manual flip
-				this.data("flipedRotate", "rotatex(180deg)");
-			}else{
-				this.data("flipedRotate", "rotatey(180deg)");
-			}
-			var flipedRotate = this.data("flipedRotate");
+        if (settings.axis.toLowerCase() == "x") {
+          prespective = height*2;
+          // save rotating css to DOM for manual flip
+          $dom.data("flipedRotate", "rotatex(180deg)");
+        } else {
+          prespective = width*2;
+          $dom.data("flipedRotate", "rotatey(180deg)");
+        }
+        var flipedRotate = $dom.data("flipedRotate");
 
-			this.wrap("<div class='flip'></div>");
-			this.parent().css({
-				perspective: prespective,
-				position: "relative",
-				width: width,
-				height: height
-			});
+        $dom.wrap("<div class='flip'></div>");
+        $dom.parent().css({
+          perspective: prespective,
+          position: "relative",
+          width: width,
+          height: height,
+          margin: margin
+        });
 
-			this.css({
-				"transform-style": "preserve-3d",
-				transition: "all 0.5s ease-out"
-			});
+        $dom.css({
+          "transform-style": "preserve-3d",
+          transition: "all 0.5s ease-out"
+        });
 
-			this.find(".front").wrap("<div class='front-wrap'></div>");
-			this.find(".back").wrap("<div class='back-wrap'></div>");
+        $dom.find(".front").wrap("<div class='front-wrap'></div>");
+        $dom.find(".back").wrap("<div class='back-wrap'></div>");
 
-			this.find(".front, .back").css({
-				width: "100%",
-				height: "100%"
-			});
+        $dom.find(".front, .back").css({
+          width: "100%",
+          height: "100%",
+          display: 'inline-table'
+        });
 
-			this.find(".front-wrap, .back-wrap").css({
-				position: "absolute",
-				"backface-visibility": "hidden",
-				width: "100%",
-				height: "100%"
-			})
+        $dom.find(".front-wrap, .back-wrap").css({
+          position: "absolute",
+          "backface-visibility": "hidden",
+          width: "100%",
+          height: "100%"
+        });
 
-			this.find(".back-wrap").css({
-				transform: flipedRotate
-			})
+        $dom.find(".back-wrap").css({
+          transform: flipedRotate
+        });
 
-			if(settings.trigger.toLowerCase() == "click"){
-				this.parent().click(function(){
-					if(jqObj.data("fliped")){
-						unflip(jqObj);
-		      }else{
-		      	flip(jqObj, flipedRotate);
-		      }
-				});
-			}else if(settings.trigger.toLowerCase() == "hover"){
-				this.parent().hover(function(){
-					flip(jqObj, flipedRotate);
-				}, function(){
-					unflip(jqObj);
-				});
-			}	
-  	}
-		// Return jQuery so that it's chainable 
-		return this;		
+        if (settings.trigger.toLowerCase() == "click") {
+          $dom.parent().click(function() {
+	          if ($dom.data("fliped")) {
+	            unflip($dom);
+	          } else {
+	            flip($dom, flipedRotate);
+	          }
+	        });
+        } else if (settings.trigger.toLowerCase() == "hover") {
+          $dom.parent().hover(function() {
+          	flip($dom, flipedRotate);
+        	}, function() {
+        		unflip($dom);
+        	});
+        }
+      }
+    });
+
+    return this;
   };
  
 }( jQuery ));
