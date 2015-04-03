fc856218c490d57239fb220f88735b09edd4e64f
Nattawat Nonsung <armmer1@gmail.com>
2015-04-04 00:21:57 +1100

    Update flip to v1.0.1

--- asset/js/jquery.flip.js
+++ asset/js/jquery.flip.js
@@ -1,19 +1,27 @@
-/*! flip - v1.0.0 - 2015-01-19
-* https://github.com/nnattawat/flip
-* Copyright (c) 2015 Nattawat Nonsung; Licensed MIT */
-
 (function( $ ) {
-  var flip = function(dom, flipedRotate) {
-    dom.data("fliped", true);
-    dom.css({
-      transform: flipedRotate
+  var flip = function($dom) {
+    $dom.data("fliped", true);
+
+    var rotateAxis = "rotate" + $dom.data("axis");
+    $dom.find(".front").css({
+      transform: rotateAxis + ($dom.data("reverse") ? "(-180deg)" : "(180deg)")
+    });
+
+    $dom.find(".back").css({
+      transform: rotateAxis + "(0deg)"
     });
   };
 
-  var unflip = function(dom) {
-    dom.data("fliped", false);
-    dom.css({
-      transform: "rotatex(0deg)"
+  var unflip = function($dom) {
+    $dom.data("fliped", false);
+
+    var rotateAxis = "rotate" + $dom.data("axis");
+    $dom.find(".front").css({
+      transform: rotateAxis + "(0deg)"
+    });
+
+    $dom.find(".back").css({
+      transform: rotateAxis + ($dom.data("reverse") ? "(180deg)" : "(-180deg)")
     });
   };
 
@@ -23,7 +31,7 @@
 
       if (options !== undefined && typeof(options) == "boolean") { // Force flip the DOM
         if (options) {
-          flip($dom, $dom.data("flipedRotate"));
+          flip($dom);
         } else {
           unflip($dom);
         }
@@ -34,50 +42,39 @@
           trigger: "click",
           speed: 500
         }, options );
-
-        var prespective;
-        var direction = settings.reverse? "-180deg" : "180deg";
         
+        // save reverse and axis css to DOM for performing flip
+        $dom.data("reverse", settings.reverse);
+        $dom.data("axis", settings.axis);
+
         if (settings.axis.toLowerCase() == "x") {
-          prespective = $dom.outerHeight() * 2;
-          // save rotating css to DOM for manual flip
-          $dom.data("flipedRotate", "rotatex(" + direction + ")");
+          var prespective = $dom.outerHeight() * 2;
+          var rotateAxis = "rotatex";
         } else {
-          prespective = $dom.outerWidth() * 2;
-          $dom.data("flipedRotate", "rotatey(" + direction + ")");
+          var prespective = $dom.outerWidth() * 2;
+          var rotateAxis = "rotatey";
         }
-        var flipedRotate = $dom.data("flipedRotate");
-
-        $dom.wrap("<div class='flip'></div>");
-        $dom.parent().css({
-          perspective: prespective,
-          position: "relative",
-          "margin-top": $dom.css("margin-top"),
-          "margin-bottom": $dom.css("margin-bottom"),
-          "margin-left": $dom.css("margin-left"),
-          "margin-right": $dom.css("margin-right"),
-          width: $dom.outerWidth(),
-          height: $dom.outerHeight()
-        });
-
-        var speedInSec = settings.speed/1000 || 0.5;
-        $dom.css({
-          "transform-style": "preserve-3d",
-          transition: "all " + speedInSec + "s ease-out",
-          margin: '0px'
-        });
-
-        $dom.find(".front, .back").outerHeight($dom.height());
-        $dom.find(".front, .back").outerWidth($dom.width());
-
-        $dom.find(".front, .back").css({
-          position: "absolute",
-          "backface-visibility": "hidden"
-        });
 
         $dom.find(".back").css({
-          transform: flipedRotate
+          transform: rotateAxis + "(" + (settings.reverse? "180deg" : "-180deg") + ")"
         });
+
+        $dom.css({
+          perspective: prespective,
+          position: "relative"
+        });
+
+        var speedInSec = settings.speed / 1000 || 0.5;
+        $dom.find(".front, .back")
+          .outerHeight($dom.height())
+          .outerWidth($dom.width())
+          .css({
+            "transform-style": "preserve-3d",
+            position: "absolute",
+            transition: "all " + speedInSec + "s ease-out",
+            "backface-visibility": "hidden"
+          });
+
 
         if (settings.trigger.toLowerCase() == "click") {
           $dom.find('button, a, input[type="submit"]').click(function (event) {
@@ -88,14 +85,14 @@
             if ($dom.data("fliped")) {
               unflip($dom);
             } else {
-              flip($dom, flipedRotate);
+              flip($dom);
             }
           });
         } else if (settings.trigger.toLowerCase() == "hover") {
           var performFlip = function() {
             $dom.unbind('mouseleave', performUnflip);
 
-            flip($dom, flipedRotate);
+            flip($dom);
 
             setTimeout(function() {
               $dom.bind('mouseleave', performUnflip);
